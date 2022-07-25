a38ca0fd8530d465ae1aac68d4cdfb46237e8a4f
JavadiMajid <Majid.Javadi.Dev00@hotmail.com>
2022-07-25 13:04:30 +0430

    test2

--- src/Services/Catalog/Catalog.API/Data/CatalogContextSeed.cs
+++ src/Services/Catalog/Catalog.API/Data/CatalogContextSeed.cs
@@ -16,7 +16,7 @@ namespace Catalog.API.Data
             {
                 productCollection.InsertManyAsync(GetPreconfiguredProducts());
             }
-        }
+        }  
 
         private static IEnumerable<Product> GetPreconfiguredProducts()
         {
